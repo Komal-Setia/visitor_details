1cbe696652246d258f05ffdc30445bc3a75ebdaa
Komal <komal0527.cse19@chitkara.edu.in>
2021-10-06 23:39:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
   }
   
-  
-  
+
   const express=require('express');
   const app=express();
   const path=require('path');
   const bodyParser=require('body-parser');
-  const nodemailer=require('nodemailer');
-  const SendmailTransport = require('nodemailer/lib/sendmail-transport');
+  const mongoose = require("mongoose");
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const { json } = require('body-parser');
   
   app.set('view engine','ejs');
   app.set('views',path.join(__dirname,'views'));
   app.use(express.static(path.join(__dirname,'public')));
   app.use(express.urlencoded({extended:true}));
+
+  const User = require("./models/schema.js");
   
   
   app.get('/', (req,res)=>{
@@ -23,57 +25,44 @@ if(process.env.NODE_ENV !== 'production'){
   });
   
   app.post('/send', (req, res) => {
-      const output = `
-        <p>Welcome!</p>
-        <h3>Visitor Details</h3>
-        <ul>  
-          <li>Name: ${req.body.name}</li>
+    var email = req.body.email;
+	var checkIn = req.body.checkIn;
+
+	const result = new User({
+		name: req.body.name,
+		email: req.body.email,
+		phone: req.body.phone,
+		checkIn: req.body.checkIn,
+	});
+
+	result.save();
+	const d = new Date();
+	const message = {
+		to: email,
+		from: process.env.MY_EMAIL,
+		subject: "Creted By Komal",
+		html: `<h1>Your check in time at Office is ${d.getHours()}:${d.getMinutes()} </h1>
+            <ul>
+            <li>Name: ${req.body.name}</li>
           <li>Email: ${req.body.email}</li>
           <li>Phone: ${req.body.phone}</li>
-          <li>Your Check-In time is: ${req.body.time}</li>
           <li>Availability status:True</li>
-        </ul>
-      
-      `;
-    
-    
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        port: 587,
-        secure: false, 
-        auth: {
-          type:'OAuth2',
-            user: process.env.MY_EMAIL, 
-            pass: process.env.MY_PASSWORD
-        },
-        tls:{
-          rejectUnauthorized:false
-        }
-      });
-    
-      // setup email data with unicode symbols
-      let mailOptions = {
-          from: '"Komal Setia" <komalsetia2001@gmail.com>', 
-          to: `${req.body.email}`, 
-          subject: 'Hello From Komal',
-          text: 'Your Check-in details', 
-          html: output 
-      };
-    
-    
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message sent: %s', info.messageId);   
-          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-          res.render('sent');
-    
-      });
-      });
-  
-  
-  
+            </ul>`,
+	};
+
+	sgMail.send(message)
+		.then((res) => {
+      res.render("sent");
+			console.log("Sent Successfully");
+	
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+
+
   
   
   
@@ -82,4 +71,18 @@ if(process.env.NODE_ENV !== 'production'){
     
     console.log('Server started...')
 });
-  
+
+const DB='mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.eb1wd.mongodb.net/visitor-app?retryWrites=true&w=majority'; 
+ const connectDB = async () => {
+	try {
+		await mongoose.connect(DB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+    
+		});
+		console.log("Mongo DB connected");
+	} catch (err) {
+		console.error(err.message);
+	}
+};
+connectDB();
